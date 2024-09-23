bce84553495c77a075e787c38fba8fced8641b49
Pauline Fugit <paulinefugit@gmail.com>
2024-09-23 16:03:04 -0300

    formsapi

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -11,57 +11,24 @@ export async function POST(request) {
             data: data
         })
 
-        // Configurar el transporte de correo
+        // Configurar el transporte de correo usando Sendmail
         let transporter = nodemailer.createTransport({
-            host: "c1961437.ferozo.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                rejectUnauthorized: false
-            },
-            connectionTimeout: 10000, // 10 seconds
-            greetingTimeout: 5000, // 5 seconds
+            sendmail: true,
+            path: '/usr/sbin/sendmail.sendmail',  // La ruta proporcionada por Ferozo
         })
 
-        // Verificar la conexión
-        await new Promise((resolve, reject) => {
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
-
         // Enviar correo
-        await new Promise((resolve, reject) => {
-            transporter.sendMail({
-                from: '"Asesorías Valdivia" <' + process.env.EMAIL_USER + '>',
-                to: process.env.EMAIL_USER, // tu dirección de correo
-                subject: "Nuevo mensaje de contacto",
-                text: `
-                    Nombre: ${data.nombre} ${data.apellido}
-                    Correo: ${data.correo}
-                    Teléfono: ${data.telefono}
-                    Mensaje: ${data.mensaje}
-                `,
-            }, (err, info) => {
-                if (err) {
-                    console.error("Error sending email:", err);
-                    reject(err);
-                } else {
-                    console.log("Email sent successfully:", info);
-                    resolve(info);
-                }
-            });
-        });
+        await transporter.sendMail({
+            from: '"Asesorías Valdivia" <no-reply@asesoriasvaldivia.cl>',
+            to: process.env.EMAIL_USER, // Tu dirección de correo o la de destino
+            subject: "Nuevo mensaje de contacto",
+            text: `
+                Nombre: ${data.nombre} ${data.apellido}
+                Correo: ${data.correo}
+                Teléfono: ${data.telefono}
+                Mensaje: ${data.mensaje}
+            `,
+        })
 
         console.log("Formulario de contacto enviado y correo enviado exitosamente.")
         return new NextResponse(JSON.stringify(contactForm), {
@@ -70,7 +37,7 @@ export async function POST(request) {
         })
     } catch (error) {
         console.error("Error en la API de contacto:", error)
-        return new NextResponse(JSON.stringify({ error: error.message }), { 
+        return new NextResponse(JSON.stringify({ error: error.message }), {
             status: 500,
             headers: { "Content-Type": "application/json" }
         })
--- src/app/api/cotization/route.js
+++ src/app/api/cotization/route.js
@@ -11,58 +11,27 @@ export async function POST(request) {
             data: data
         })
 
-        // Configurar el transporte de correo
+        // Configurar el transporte de correo usando Sendmail
         let transporter = nodemailer.createTransport({
-            host: "c1961437.ferozo.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                rejectUnauthorized: false
-            },
-            connectionTimeout: 10000, // 10 seconds
-            greetingTimeout: 5000, // 5 seconds
+            sendmail: true,
+            path: '/usr/sbin/sendmail.sendmail',  // La ruta proporcionada por Ferozo
         })
 
-        // Verificar la conexión
-        await new Promise((resolve, reject) => {
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
-
         // Enviar correo
-        await new Promise((resolve, reject) => {
-            transporter.sendMail({
-                from: '"Asesorías Valdivia" <' + process.env.EMAIL_USER + '>',
-                to: process.env.EMAIL_USER, // tu dirección de correo
-                subject: "Nuevo mensaje para cotizar un servicio",
-                text: `
-                    Nombre: ${data.nombre} ${data.apellido}
-                    Correo: ${data.correo}
-                    Teléfono: ${data.telefono}
-                    Servicio: ${data.servicio}
-                    Mensaje: ${data.mensaje}
-                `,
-            }, (err, info) => {
-                if (err) {
-                    console.error("Error sending email:", err);
-                    reject(err);
-                } else {
-                    console.log("Email sent successfully:", info);
-                    resolve(info);
-                }
-            });
-        });
+        await transporter.sendMail({
+            from: '"Asesorías Valdivia" <no-reply@asesoriasvaldivia.cl>',
+            to: process.env.EMAIL_USER, // Tu dirección de correo o la de destino
+            subject: "Nuevo mensaje de contacto",
+            text: `
+                Nombre: ${data.nombre} ${data.apellido}
+                Correo: ${data.correo}
+                Teléfono: ${data.telefono}
+                Servicio: ${data.servicio}
+                Mensaje: ${data.mensaje}
+            `,
+        })
+
+        
 
         console.log("Formulario de contacto enviado y correo enviado exitosamente.")
         return new NextResponse(JSON.stringify(cotizationForm), {
