9e36754b6403cb6620fd9123743bbbae38d280bd
Pauline Fugit <paulinefugit@gmail.com>
2024-10-05 04:30:22 -0300

    fix register form

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { useState } from "react"
@@ -69,11 +70,149 @@ function RegisterPage() {
           <h1 className="text-custom-blue font-bold text-4xl mb-4">
             Registro de Usuarios
           </h1>
+                <label htmlFor="username" className="text-slate-500 mb-2 block  text-sm" >Usuario</label>
+                <input type="text"
+                    {...(register("username", {
+                        required: {
+                            value: true,
+                            message: 'Usuario requerido'
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="Nombre de usuario"
+                />
+                {errors.username && 
+                (<span className="text-red-500 text-sm">{errors.username.message}</span>)}
 
-          {/* Existing form fields */}
-          {/* ... */}
+                <label htmlFor="email" className="text-slate-500 mb-2 block  text-sm" >Correo Electrónico:</label>
+                <input type="email"
+                    {...(register("email", {
+                        required: {
+                            value: true,
+                            message: "Correo electrónico requerido.",
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="correo@ejemplo.cl"
 
-          <label htmlFor="image" className="text-slate-500 mb-2 block text-sm">
+                />
+                {
+                    errors.email && (
+                        <span
+                            className="text-red-500 text-sm">{errors.email.message}</span>
+                    )
+                }
+
+                <label htmlFor="name" className="text-slate-500 mb-2 block  text-sm" >Nombre:</label>
+                <input type="text"
+                    {...(register("name", {
+                        required: {
+                            value: true,
+                            message: 'Nombre requerido'
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="Nombre del Usuario"
+                />
+                {
+                    errors.username && (
+                        <span
+                            className="text-red-500 text-sm">{errors.username.message}</span>
+                    )
+                }
+
+                <label htmlFor="lastname" className="text-slate-500 mb-2 block  text-sm" >Apellido:</label>
+                <input type="text"
+                    {...(register("lastname", {
+                        required: {
+                            value: true,
+                            message: 'Apellido requerido'
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="Apellido del Usuario"
+                />
+                {
+                    errors.username && (
+                        <span
+                            className="text-red-500 text-sm">{errors.username.message}</span>
+                    )
+                }
+
+                <label htmlFor="password" className="text-slate-500 mb-2 block  text-sm" >Contraseña:</label>
+                <input type="password"
+                    {...(register("password", {
+                        required: {
+                            value: true,
+                            message: "Contraseña requerida."
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="**********"
+
+                />
+                {
+                    errors.password && (
+                        <span
+                            className="text-red-500 text-sm">{errors.password.message}</span>
+                    )
+                }
+
+                <label htmlFor="confirmPassword" className="text-slate-500 mb-2 block  text-sm" >Confirma tu contraseña:</label>
+                <input type="password"
+                    {...(register("confirmPassword", {
+                        required: {
+                            value: true,
+                            message: "Confirmación de contraseña requerida."
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="**********"
+
+                />
+                {
+                    errors.confirmPassword && (
+                        <span
+                            className="text-red-500 text-sm">{errors.confirmPassword.message}</span>
+                    )
+                }
+                 <label htmlFor="phone" className="text-slate-500 mb-2 block  text-sm" >Teléfono:</label>
+                <input type="text"
+                    {...(register("phone", {
+                        required: {
+                            value: true,
+                            message: "Teléfono requerido."
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="123456789"
+
+                />
+                {
+                    errors.phone && (
+                        <span
+                            className="text-red-500 text-sm">{errors.phone.message}</span>
+                    )
+                }
+
+                <label htmlFor="role" className="text-slate-500 mb-2 block  text-sm" >Ingresa el rol del usuario</label>
+                <input type="text"
+                    {...(register("role", {
+                        required: {
+                            value: true,
+                            message: "Rol de usuario requerido."
+                        }
+                    }))}
+                    className="p-3 rounded block mb-2 bg-custom-white text-custom-blue w-full"
+                    placeholder="Define al usuario como TEAM O CLIENT"
+                />
+                {
+                    errors.role && (
+                        <span
+                            className="text-red-500 text-sm">{errors.role.message}</span>
+                    )
+                }
+                <label htmlFor="image" className="text-slate-500 mb-2 block text-sm">
             Imagen de perfil:
           </label>
           <input
@@ -101,4 +240,3 @@ function RegisterPage() {
 }
 
 export default RegisterPage;
-
